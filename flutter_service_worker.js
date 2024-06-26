94e8608b5ace6ff0d4bc80f304a9c9fbc2784619
scrii <scrii@users.noreply.github.com>
2024-06-26 16:42:38 +0000

    deploy: c02f2c30ca397963e6f6a6316ba4a15fdc542e30

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "867d266acff150df713e75963e4d9d40
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "6897b0e4165dcae6575d5d5a31b27bef",
+"flutter_bootstrap.js": "9dfbef8625c98dbf778f24a1bc5ba397",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
